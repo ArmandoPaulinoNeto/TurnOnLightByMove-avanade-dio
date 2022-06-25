ee0fe22ddcd897de82de1b1cad3f02ead924d1b2
ArmandoPaulinoNeto <armandopaulinosilvaneto@gmail.com>
2022-06-24 21:36:32 -0300

    First commit: Subindo o app com nudança de status ao clicar nos icons.

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import { SafeAreaView, Image, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+import lightOff from './assets/icons/eco-light-off.png';
+import light from './assets/icons/eco-light.png';
+import dio from './assets/icons/logo-dio.png';
+import dioWhite from './assets/icons/logo-dio-white.png';
+
+const App = () => {
+
+  const [toggle, setToggle] = useState(false);
+  const handleChandeStatus = () => setToggle(oldToggle => !oldToggle);
+
+  return (
+            <SafeAreaView style={[style.container, toggle ? style.bgLight: style.bgDark]}>
+              <TouchableOpacity 
+                onPress={handleChandeStatus}
+              >
+                <View>                
+                  <Image style={style.lightDefault} source={toggle ? light : lightOff}/>
+                  <Image style={style.lightDefault} source={toggle ? dio : dioWhite}/>                
+                </View>
+              </TouchableOpacity>
+              
+            </SafeAreaView>
+          );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bgLight: {
+    backgroundColor: 'white',
+  },
+  bgDark: {
+    backgroundColor: 'black',
+  },
+  lightDefault:{
+    width: 250,
+    height: 250,
+    resizeMode: 'contain'
+  }
+});
