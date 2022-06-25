332abe970ce7af51c1d07ef34ac8484408bf6486
ArmandoPaulinoNeto <armandopaulinosilvaneto@gmail.com>
2022-06-24 22:29:33 -0300

    App ligado e desligado pela mudança de status.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
-import { SafeAreaView, Image, View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { SafeAreaView, Image, View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import Torch from 'react-native-torch';
 
 import lightOff from './assets/icons/eco-light-off.png';
 import light from './assets/icons/eco-light.png';
@@ -9,19 +10,20 @@ import dioWhite from './assets/icons/logo-dio-white.png';
 const App = () => {
 
   const [toggle, setToggle] = useState(false);
+  useEffect(() => {
+    Torch.switchState(toggle);
+    Alert.alert(toggle ? 'Lenterna ligada' : 'Lenterna desligada');
+  }, [toggle]);
   const handleChandeStatus = () => setToggle(oldToggle => !oldToggle);
 
   return (
             <SafeAreaView style={[style.container, toggle ? style.bgLight: style.bgDark]}>
-              <TouchableOpacity 
-                onPress={handleChandeStatus}
-              >
+              <TouchableOpacity onPress={handleChandeStatus}>
                 <View>                
                   <Image style={style.lightDefault} source={toggle ? light : lightOff}/>
                   <Image style={style.lightDefault} source={toggle ? dio : dioWhite}/>                
                 </View>
-              </TouchableOpacity>
-              
+              </TouchableOpacity>              
             </SafeAreaView>
           );
 };
